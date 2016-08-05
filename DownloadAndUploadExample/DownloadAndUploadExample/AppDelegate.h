9394460230449e946bffee918ecfddf926cda684
Owen.li <Owen@OwendeMacBook-Pro.local>
2016-08-05 15:50:47 +0800

    提交项目

---
+++ DownloadAndUploadExample/DownloadAndUploadExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DownloadAndUploadExample
+//
+//  Created by Owen.li on 16/8/5.
+//  Copyright © 2016年 owen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DownloadAndUploadExample/DownloadAndUploadExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DownloadAndUploadExample
+//
+//  Created by Owen.li on 16/8/5.
+//  Copyright © 2016年 owen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
